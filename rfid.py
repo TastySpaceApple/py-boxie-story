87566b894e15804673ad153986de05b34315c91c
IdanI <idan.itzhaky@gmail.com>
2025-11-22 14:06:06 +0200

    meh

--- rfid.py
+++ rfid.py
@@ -7,18 +7,25 @@ reader = SimpleMFRC522()
 character = "cat"
 rfidText = "בית קפה"
 
+texts = {
+  584189665081: "בית קפה"
+  584189665080: "גן"
+  584189665079: "בריכה"
+  584189665078: "יער"
+}
+
 lastID = None
 
 try:
   while True:
     print("Place your RFID tag near the reader...")
-    id, text = reader.read()
+    id, _ = reader.read()
     if id != lastID:
       lastID = id
       if(id == None):
         continue
-      print(f"RFID tag detected with ID: {id} and text: {text}")
-      user_message = text.strip()
+      user_message = texts.get(id, "לא ידוע")
+      print(f"RFID Tag Detected. ID: {id}, Text: {user_message}")
       boxie_reply = boxie.talk(character, user_message)
     print(f"ID: {id}")
     print(f"Text: {text}")
